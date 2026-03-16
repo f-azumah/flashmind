a96afc44dae954feefe23fbf16e29cadc66b1f93
Fiona Azumah <fifs@Fionas-MacBook-Pro-2.local>
2026-03-16 00:17:34 -0400

    Initial Spring Boot setup

---
+++ src/main/java/com/fiona/flashmind/FlashmindApplication.java
@@ -0,0 +1,13 @@
+package com.fiona.flashmind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashmindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashmindApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiona/flashmind/FlashmindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiona.flashmind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashmindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
